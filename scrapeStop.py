00d8abd2bdd030c55c7f0a1f94d1dbe7b9ab3b32
Jarek Piórkowski <jarek@piorkowski.ca>
2012-05-19 15:56:42 -0700

    generalize and refactor stop scraping

--- scrapeStop.py
+++ scrapeStop.py
@@ -2,40 +2,84 @@
 
 import pycurl, StringIO, json, urllib
 
+# constants for Translink scraping
 
-# URLs
-htmlURL = 'http://nb.translink.ca/text/stop/{STOP}/route/{ROUTE}'
-jsonURL = 'http://nb.translink.ca/rideapi.ashx?cp=gssr%2F{CODE}==;{ROUTE}'
+HTMLstopURL = 'http://nb.translink.ca/text/stop/{STOP}'
+HTMLstopAndRouteURL = 'http://nb.translink.ca/text/stop/{STOP}/route/{ROUTE}'
+
+JSONstopURL = 'http://nb.translink.ca/rideapi.ashx?cp=gsas%2F{CODE}=='
+JSONstopAndRouteURL = 'http://nb.translink.ca/rideapi.ashx?cp=gssr%2F{CODE}==;{ROUTE}'
 
 
-c = pycurl.Curl()
-c.setopt(pycurl.COOKIEFILE, 'cookie.txt')
-c.setopt(pycurl.URL, htmlURL.replace('{STOP}', '50166').replace('{ROUTE}', '003'))
-b = StringIO.StringIO()
-c.setopt(pycurl.WRITEFUNCTION, b.write)
-c.perform()
+def getCurlObject():
+	c = pycurl.Curl()
+	c.setopt(pycurl.COOKIEFILE, 'cookie.txt')
 
-html = b.getvalue()
-b.close()
-b = StringIO.StringIO() # clear
-c.setopt(pycurl.WRITEFUNCTION, b.write)
+	return c
 
-codeIndexStart = html.find('textSearchStopAndRoute(\'') + 24
-codeIndexEnd = codeIndexStart + 22
-code = html[codeIndexStart:codeIndexEnd]
-code = urllib.quote_plus(code)
+def getURL(c, url):
+	c.setopt(pycurl.URL, url)
 
-c.setopt(pycurl.URL, jsonURL.replace('{CODE}', code).replace('{ROUTE}', '003'))
-c.perform()
+	b = StringIO.StringIO()
+	c.setopt(pycurl.WRITEFUNCTION, b.write)
+	c.perform()
 
-jsonString = b.getvalue()
+	html = b.getvalue()
+	b.close()
+	
+	return html
 
-data = json.loads(jsonString)
-departures = data['NextBuses'][0]['Schedules']
+def getStopCode(c, stopNumber):
+	html = getURL(c, HTMLstopURL.replace('{STOP}', stopNumber))
 
-for departure in departures:
-	print departure['ExpectedCountdown'],
-	if (departure['Source'] != 'et'):
-		print '*',
-	print
+	codeIndexStart = html.find('textSearchStop(\'') + 16
+	codeIndexEnd = codeIndexStart + 22
+	code = html[codeIndexStart:codeIndexEnd]
+	code = urllib.quote_plus(code)
 
+	return code
+
+def getStopAndRouteCode(c, stopNumber, route):
+	html = getURL(c, HTMLstopAndRouteURL.replace('{STOP}', stopNumber).replace('{ROUTE}', route))
+
+	codeIndexStart = html.find('textSearchStopAndRoute(\'') + 24
+	codeIndexEnd = codeIndexStart + 22
+	code = html[codeIndexStart:codeIndexEnd]
+	code = urllib.quote_plus(code)
+
+	return code
+
+def getStopJSON(stopNumber, route = False):
+	c = getCurlObject()
+
+	if route == False:
+		code = getStopCode(c, stopNumber)
+		json = getURL(c, JSONstopURL.replace('{CODE}', code))
+		
+	else:
+		code = getStopAndRouteCode(c, stopNumber, route)
+		json = getURL(c, JSONstopAndRouteURL.replace('{CODE}', code).replace('{ROUTE}', route))
+
+	return json
+
+def parseStopJSON(jsonString):
+	data = json.loads(jsonString)
+	schedules = dict()
+
+	for route in data['NextBuses']:
+		departures = []
+		for departure in route['Schedules']:
+			if (departure['Source'] != 'et'):
+				departures.append(str(departure['ExpectedCountdown']) + '*')
+			else:
+				departures.append(str(departure['ExpectedCountdown']))
+		schedules[route['RouteNo']] = departures
+
+	return schedules
+
+
+#print getStopJSON('50166')
+#print getStopJSON('50166', '003')
+
+print parseStopJSON(getStopJSON('50166', '003'))
+print parseStopJSON(getStopJSON('50181'))
